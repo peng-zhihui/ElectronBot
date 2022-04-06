5a0e16afc59694b7f195623b7dd99fccaedcab44
peng-zhihui <593245898@qq.com>
2022-04-06 13:57:16 +0800

    [Fw] Fix USB descriptor DeviceClass issue.

--- 2.Firmware/ElectronBot-fw/USB_DEVICE/App/usbd_desc.c
+++ 2.Firmware/ElectronBot-fw/USB_DEVICE/App/usbd_desc.c
@@ -167,7 +167,7 @@ __ALIGN_BEGIN uint8_t USBD_HS_DeviceDesc[USB_LEN_DEV_DESC] __ALIGN_END =
 #endif /* (USBD_LPM_ENABLED == 1) */
 
   0x02,
-  0x00,                       /*bDeviceClass*/
+  0x02,                       /*bDeviceClass*/
   0x00,                       /*bDeviceSubClass*/
   0x00,                       /*bDeviceProtocol*/
   USB_MAX_EP0_SIZE,           /*bMaxPacketSize*/
